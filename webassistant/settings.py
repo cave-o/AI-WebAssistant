62481ac583928c3a2a9c6b930b6db777211ecc98
cave-o <97689028+cave-o@users.noreply.github.com>
2023-04-05 12:43:35 -0700

    fix: modified settings

--- webassistant/settings.py
+++ webassistant/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure--#%kkh%cun3=)m2^u3e9ce6lkye%im7u4*(1=$ct@er^6=9drc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['ai-web-assistant.vercel.app']
 
 
 # Application definition
